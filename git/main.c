0e88709a9233f2eb957addf9795405883f08e085
Dada <daniel211920@gmail.com>
2016-11-08 21:08:20 +0800

    Initial Commit

---
+++ git/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  git
+//
+//  Created by Dada on 2016/11/8.
+//  Copyright © 2016年 Dada. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
